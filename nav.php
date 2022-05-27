4f70ee8575eaf368822aca132324db7d4c637a5f
JoanLuzano <Joanmarieluzano@gmail.com>
2022-05-27 09:13:26 +0800

    Second Update
    Update of initial file(s)

---
+++ nav.php
@@ -0,0 +1,10 @@
+<div align="center" id="banner"><img src="banner_project.gif" width="960" height="160" alt="banner"/></div>
+<div align="center" h1 style="background-color:lavenderblush; font-size:22px;">
+<a class="active" href="logcall.php">Log Call</a>
+<a href="update.php">Update</a>
+<a href="#">Report</a>
+<a href="#">History</a>
+</div>
+<body bgcolor="#FFE5AF"
+	
+</body>
